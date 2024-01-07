09ec0d06c4aff0cb412251d0a26b8dc4cb241ea1
Norbi-S <nbitxd@gmail.com>
2024-01-07 19:30:57 +0000

    Updated trouble keymappings

--- nvim/after/plugin/trouble.lua
+++ nvim/after/plugin/trouble.lua
@@ -1,2 +1,3 @@
 -- Toggle trouble
-vim.keymap.set("n", "<leader>t", function() require("trouble").toggle() end)
+vim.keymap.set("n", "<leader>tt", function() require("trouble").open() end)
+vim.keymap.set("n", "<leader>tq", function() require("trouble").close() end)
