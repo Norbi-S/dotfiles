e047a4d55dcf9dc68b01fee7d43f0c4a84a1f078
Norbi-S <nbitxd@gmail.com>
2024-01-08 22:15:56 +0000

    Change trouble shortcut

--- nvim/after/plugin/trouble.lua
+++ nvim/after/plugin/trouble.lua
@@ -1,3 +1,2 @@
 -- Toggle trouble
-vim.keymap.set("n", "<leader>tt", function() require("trouble").open() end)
-vim.keymap.set("n", "<leader>tq", function() require("trouble").close() end)
+vim.keymap.set("n", "<leader>t", function() require("trouble").toggle() end)
