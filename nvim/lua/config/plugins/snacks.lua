e5ad8cf99d7cb004cad6df5d48b9873e38d3b1e4
Norbi-S <nbitxd@gmail.com>
2026-06-09 00:53:40 +0100

    Allow inlayhints in snacks

--- nvim/lua/config/plugins/snacks.lua
+++ nvim/lua/config/plugins/snacks.lua
@@ -156,7 +156,7 @@ return {
           --   "<leader>uc")
           -- Snacks.toggle.treesitter():map("<leader>uT")
           -- Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
-          -- Snacks.toggle.inlay_hints():map("<leader>uh")
+          Snacks.toggle.inlay_hints():map("<leader>uh")
           -- Snacks.toggle.indent():map("<leader>ug")
           -- Snacks.toggle.dim():map("<leader>uD")
         end,
